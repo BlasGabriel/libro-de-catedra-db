2e2971e2620600b5cafd3fe564a20347e947dda3
Gabriel <blasgabriel200@gmail.com>
2023-10-02 18:03:29 -0300

    a

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require("sequelize");
-const sequelize_db = new Sequelize("railway", "root", "Lz4OO1FdQoi3LaDLds4h", {
+const sequelize_db = new Sequelize("railway", "root", "Q2vAe4tqZ1Dchtbiojf1", {
 // const sequelize_db = new Sequelize("libro_de_catedra_db", "root", "mysql", {
   // host: "localhost",
-  host: "containers-us-west-56.railway.app",
+  host: "containers-us-west-74.railway.app",
   dialect: "mysql",
-  port: "5482",
+  port: "7405",
   timezone: "-04:00", // Configura la zona horaria para Paraguay
 });
 
